e7964d566055e6b90a0592a463cfcaa010082d2e
nkellem <noahkellem@gmail.com>
2018-03-26 15:09:04 -0400

    Fixed image filepath

--- client/app/maker.js
+++ client/app/maker.js
@@ -55,7 +55,7 @@ const DomoList = function(props) {
   const domoNodes = props.domos.map(function(domo) {
     return (
       <div key={domo._id} className="domo">
-        <img src="./img/domoFace.jpeg" alt="domo face" className="domoFace"/>
+        <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace"/>
         <h3 className="domoName"> Name: {domo.name} </h3>
         <h3 className="domoAge"> Age: {domo.age} </h3>
       </div>
